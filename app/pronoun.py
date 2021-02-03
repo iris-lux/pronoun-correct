057e2873f7580e5a77c45bcc888b05eaed733311
Iris Lux <iris.lux0@gmail.com>
2021-02-03 15:12:04 -0800

    created pluralize present heads to handle grmatically plural pronouns such as they

--- app/pronoun.py
+++ app/pronoun.py
@@ -2,10 +2,21 @@
 
 
 class Pronoun: 
-    def __init__(self, subjective, objective, possesive_weak, posessive_strong, reflexive):
+    def __init__(self, subjective, objective, possesive_weak, posessive_strong, reflexive, gramatically_plural = False):
         self.subjective = subjective
         self.objective = objective
         self.posessive_weak = possesive_weak 
         self.posessive_strong = posessive_strong
         self.reflexive = reflexive 
+        self.gramatically_plural = gramatically_plural
+        
+    def equivalent_pronoun(self, pronoun_case):
+        case_pronoun_switch = {
+            'SUBJ' : self.subjective, 
+            'OBJ' : self.objective,
+            'POSS_WK' : self.posessive_weak,
+            'POSS_STRG' : self.posessive_strong,
+            'REFLX' : self.reflexive
+        }
+        return case_pronoun_switch.get(pronoun_case)
 
--- app/routes.py
+++ app/routes.py
@@ -9,8 +9,8 @@ import en_core_web_sm
 import neuralcoref 
 
 nlp = spacy.load('en_core_web_sm')
-neuralcoref.add_to_pipe(nlp)
-
+#neuralcoref.add_to_pipe(nlp)
+neuralcoref.add_to_pipe(nlp, greedyness=0.6)
 she_her_hers = Pronoun('she', 'her', 'her', 'hers', 'herself')
 he_him_his = Pronoun('he', 'him', 'his', 'his', 'himself')
 they_them_theirs = Pronoun('they', 'them', 'their', 'theirs', 'themself')
@@ -23,8 +23,31 @@ def find_cluster(target_name, doc):
             return cluster 
     return None 
 
+def pronoun_case(pronoun_text):
+    pronoun_case_switch = {
+        'she': 'SUBJ',
+        'he': 'SUBJ',
+        'they' : 'SUBJ',
+        'her': 'OBJ',
+        'him': 'OBJ',
+        'them' : 'OBJ',
+        'their' : 'POSS_WK',
+        'his': 'POSS_STRG',
+        'hers' : 'POSS_STRG',
+        'theirs' : 'POSS_STRG',
+        'herself' : 'REFLX', 
+        'himself' : 'REFLX',
+        'themself' : 'REFLX',
+        'themselves' : 'REFLX'
+    }
+    #find better default case?
+    return pronoun_case_switch.get(pronoun_text, 'SUBJ')
+
 def replace_pronouns(orig_text, name, pronoun_replacement):
 
+    #for singular they transformation
+    present_tense_heads = []
+    pronouns = []
     doc = nlp(orig_text)
     text = ''
     buffer_start = 0
@@ -32,16 +55,71 @@ def replace_pronouns(orig_text, name, pronoun_replacement):
     if name_cluster == None:
         return 'No Match'
     #need to check if mention is pronoun
+    print(name_cluster.mentions)
+    
     for mention in name_cluster.mentions:
-        if mention[0].pos_ == 'PRON' or mention[0].pos_ == 'DET':
-            print(mention[0].dep_)
-            print(mention[0].lemma_)
-            if mention.start > buffer_start:  # If we've skipped over some tokens, let's add those in (with trailing whitespace if available)
-                text += doc[buffer_start: mention.start].text + doc[mention.start - 1].whitespace_
-            text += pronoun_replacement + doc[mention.start].whitespace_  # Replace token, with trailing whitespace if available
-            buffer_start = mention.start + 1
+        if mention[0].tag_ == 'PRP' or mention[0].tag_ == 'PRP$':
+            print(mention.text, 'tag', mention[0].tag_, spacy.explain(mention[0].tag_))
+            print(len(mention))
+            pronouns.append(mention[0])
+            
+    for mention in name_cluster.mentions:
+        # print(mention.text, 'tag', mention[0].tag_, spacy.explain(mention[0].tag_))
+        # print(len(mention))
+        if mention[0].tag_ == 'PRP' or mention[0].tag_ == 'PRP$':
+            print(mention.text, 'tag', mention[0].tag_, spacy.explain(mention[0].tag_))
+            print(len(mention))
+            if mention[0].pos_ == 'DET': 
+                replacement = pronoun_replacement.equivalent_pronoun('POSS_WK') 
+            else: 
+                replacement = pronoun_replacement.equivalent_pronoun(pronoun_case(mention[0].text.lower()))
+            
+            if(mention[0].text == mention[0].text.capitalize()):
+                replacement = replacement.capitalize()
+            
+            if(pronoun_replacement.gramatically_plural and mention[0].dep_ == 'nsubj' and mention[0].head.tag_ == 'VBZ'):
+                present_tense_heads.append(mention[0].head)
+                
+            
+            #special case for DET 
+            #print(mention[0].text, 'dep', mention[0].dep_)
+            #print(mention[0].text, 'tag', mention[0].tag_, spacy.explain(mention[0].tag_))
+            
+            #print('pos', mention[0].pos_)
+            #print('head', mention[0].head.i)
+            #print(mention[0].head.text, 'tag', mention[0].head.tag_, spacy.explain(mention[0].head.tag_))
+            # if replacement pronoun is they/them AND pronoun is the subject, add token to an array 
+            if mention[0].i > buffer_start:  # If we've skxipped over some tokens, let's add those in (with trailing whitespace if available)
+                text += doc[buffer_start: mention[0].i].text + doc[mention[0].i- 1].whitespace_
+            text += replacement + doc[mention[0].i].whitespace_  # Replace token, with trailing whitespace if available
+            buffer_start = mention[0].i + 1
+    text += doc[buffer_start:].text
+    if(len(present_tense_heads) > 0): 
+        text = pluralize_present_heads(text, present_tense_heads)
     return text
 
+def pluralize_present_heads(orig_text, heads):
+    doc = nlp(orig_text)
+    text = ''
+    buffer_start = 0
+
+    print(heads)
+    for head in heads:
+        if head.i > buffer_start:
+            text += doc[buffer_start:head.i].text + doc[head.i - 1].whitespace_ 
+
+        if head.text == 'is':
+             text += 'are' + doc[head.i].whitespace_
+        elif head.text == "'s":
+            text += "'re" + doc[head.i].whitespace_
+        else:
+            text += head.lemma_ + doc[head.i].whitespace_
+
+        buffer_start = head.i + 1 
+
+    text += doc[buffer_start:].text
+
+    return text  
 
 @app.route('/')
 @app.route('/index')
@@ -54,9 +132,14 @@ def process():
     # if request.method == 'POST':
     rawtext = request.form['rawtext']
     name = request.form['name']
-    pronoun_replacement = request.form['pronoun_replacement']
+    pronoun_option = request.form['pronoun_replacement']
+    pronoun_replacements = {
+        'she' : Pronoun('she', 'her', 'her', 'hers', 'herself'),
+        'he' : Pronoun('he', 'him', 'his', 'his', 'himself'),
+        'they' : Pronoun('they', 'them', 'their', 'theirs', 'themself', True)
+    }
 
-    output = replace_pronouns(rawtext, name, pronoun_replacement)
+    output = replace_pronouns(rawtext, name, pronoun_replacements[pronoun_option])
 
     return render_template("index.html", translated = output)
 
