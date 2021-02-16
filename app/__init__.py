10b83bd82777e171bf2586773cf90f582b660806
Iris Lux <iris.lux0@gmail.com>
2021-02-16 14:03:07 -0800

    rmv socketio

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
 from config import Config 
-from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config.from_object(Config)
-socketio = SocketIO(app)
+
 from app import pronoun
 from app import routes
 
--- pronoun-correct.py
+++ pronoun-correct.py
@@ -1,4 +1,1 @@
-from app import app, socketio
-
-if __name__ == '__main__':
-    socketio.run(app)
+from app import app
