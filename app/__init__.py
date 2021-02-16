bfbb0b080ad7e49522d2dd30f991c213b57b98f8
Iris Lux <iris.lux0@gmail.com>
2021-02-16 13:30:35 -0800

    socketio

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from config import Config 
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
+socketio = SocketIO(app)
 from app import pronoun
 from app import routes
+
